e8510d940a4b3503d3073a7a5022fb79970224d8
Theo <thodorispalios1@hotmail.com>
2021-10-01 12:19:22 +0200

    Css update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container">
     <div class="list" v-for="(post, index) in posts.Search" :key="index">
-      <img :src=post.Poster alt="poster-image">
-      <h3>{{ post.Title }}</h3>
-      <p>{{ post.Year }}</p>
+      <div class="list-item" >
+        <img :src=post.Poster alt="poster-image">
+        <div class="description">
+          <h3>{{ post.Title }}</h3>
+          <p>{{ post.Year }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,12 +18,14 @@ export default {
   data() {
     return {
       posts: [],
+      apiKey: 'f9422ce0',
+      searchTerm: 'superhero',
     }
   },
   methods: {
     async getData() {
       try {
-        let response = await fetch('https://www.omdbapi.com/?s=superhero&apikey=f9422ce0') 
+        let response = await fetch('https://www.omdbapi.com/?s='+ this.searchTerm + '&page=' + this.currentPage +'&apikey=' + this.apiKey) 
         this.posts = await response.json()
       } catch (error) {
         console.log(error)
@@ -33,12 +39,40 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+
+  .list {
+    text-decoration: none;
+    
+    .list-item {
+      height: 514px;
+      width: 322px;
+      padding: 10px;
+      box-sizing: border-box;
+      flex: 1 0 250px;
+      margin: 1rem;
+      border: 1px solid #000;
+      position: relative;
+
+      .description{
+        text-decoration: none;
+        position:absolute;
+        bottom: 0;
+        padding: 0 0 5px;
+      }
+
+      img {
+          width: 300px;
+          height: 444px
+      }
+
+      &:hover {
+        background-color: rgba(48, 48, 48, 0.1);
+      }
+    }
+  }
 }
 </style>
