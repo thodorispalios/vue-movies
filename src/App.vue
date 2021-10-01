89a1b9e3bda16449648ff56c6d46ea1763ef054b
Theo <thodorispalios1@hotmail.com>
2021-10-01 11:46:53 +0200

    Api Connected

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="container">
+    <div class="list" v-for="(post, index) in posts.Search" :key="index">
+      <img :src=post.Poster alt="poster-image">
+      <h3>{{ post.Title }}</h3>
+      <p>{{ post.Year }}</p>
+    </div>
+  </div>
 </template>
+
 
 <script>
 export default {
-  name: 'App',
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch('https://www.omdbapi.com/?s=superhero&apikey=f9422ce0') 
+        this.posts = await response.json()
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
 }
 </script>
 
